e167e28d2367e0ab70f246fc46e3f2262d401520
Eren Çolak <eren.colak01@gmail.com>
2024-01-05 09:54:12 +0300

    Onaylanma durumu için enum oluşturuldu

---
+++ Core/Ekite.Domain/Enums/ApprovalStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekite.Domain.Enums
+{
+    public enum ApprovalStatus
+    {
+        Rejected = 1, Panding , Approved 
+    }
+}
