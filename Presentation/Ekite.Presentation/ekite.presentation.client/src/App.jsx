1e64a796283cc2bb27f8406cc4f6a0aff255c189
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2024-01-09 14:09:46 +0300

    sidebar route ayarlandı

--- Presentation/Ekite.Presentation/ekite.presentation.client/src/App.jsx
+++ Presentation/Ekite.Presentation/ekite.presentation.client/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
                                     />
 
                                     <Route
-                                        path="/leave"
+                                        path="/leaves"
                                         element={<PrivateRoute element={<LeaveListPage />} />}
                                     />
                                     <Route
