94318cc37ba7963464a733badcdc99763867ed9a
Kağan Eriş <123362258+kaganeris@users.noreply.github.com>
2024-01-10 14:45:11 +0300

    avans validasyon

--- Core/Ekite.Application/Validators/AdvanceValidations/UpdateAdvanceValidator.cs
+++ Core/Ekite.Application/Validators/AdvanceValidations/UpdateAdvanceValidator.cs
@@ -13,7 +13,11 @@ namespace Ekite.Application.Validators.AdvanceValidations
 
         public UpdateAdvanceValidator()
         {
-            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama alanı boş geçilemez.");
+            RuleFor(x => x.Description).MaximumLength(250).WithMessage("En fazla 250 karakter girebilirsiniz");
+
+            RuleFor(x => x.Amount).LessThan(100000).WithMessage("Miktarı çok fazla girdiniz.");
+            RuleFor(x => x.Amount).GreaterThan(0).WithMessage("Miktarı alanı boş geçilemez");
 
         }
 
